a39ba948ebab7b37623bf674b7a29a1c1ac4df4b
Elshaday <elshadayteshome@gmail.com>
2019-04-18 09:35:39 -0700

    protected and unprotectedpages working

---
+++ app/javascript/components/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Homepage extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Homepage</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Homepage
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'protected', to: 'pages#protected'
- get 'unprotected', to: 'pages#unprotected'
+  get '*path', to: 'pages#protected', constraints: ->(request){ request.format.html? }
+ root to: 'pages#unprotected'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
