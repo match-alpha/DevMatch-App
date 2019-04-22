d4d0b9a9eea379a181300abf48bcd5ea9baa46fa
Jerrod Jordan <jerrodjordan@Tinas-MBP.localdomain>
2019-04-22 15:08:06 -0700

    Navbar Update

--- app/javascript/components/UnauthenticatedApp.js
+++ app/javascript/components/UnauthenticatedApp.js
@@ -3,6 +3,16 @@ import PropTypes from "prop-types"
 import Homepage from "./pages/Homepage"
 import { Nav, Navbar, Form, NavDropdown, FormControl, Button, Container, Jumbotron, Row, Col, Image } from "react-bootstrap"
 import Logo from "./images/Logo.png"
+import NavLogo from "./images/NavbarLogo.png"
+import Profile from "./images/Profilebw.png"
+import Melkam from "./images/m.png"
+import Jerrod from "./images/jerrod.png"
+import Dante from "./images/dante.png"
+import { HashLink as Link } from 'react-router-hash-link'
+import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor'
+
+
+configureAnchors ({offset: -80})
 
 
 
@@ -14,15 +24,17 @@ class UnauthenticatedApp extends React.Component {
 
         <div>
           <Navbar bg="primary" variant="dark" sticky="top" >
-            <Navbar.Brand href="#home">DevMatch</Navbar.Brand>
+            <Navbar.Brand img src={Logo} href="#home">
+              <img src = {NavLogo} className="d-inline-block align-top" alt="React Bootstrap logo"/>
+            </Navbar.Brand>
             <Nav className="ml-auto">
-              <Nav.Link href="#home">Mission</Nav.Link>
-              <Nav.Link href="#features">Our Tech Stack</Nav.Link>
-              <Nav.Link href="#pricing">About Us</Nav.Link>
+              <Nav.Link href="#mission">Mission</Nav.Link>
+              <Nav.Link href="#tech">Tech Stack</Nav.Link>
+              <Nav.Link href="#about">About Us</Nav.Link>
             </Nav>
           </Navbar>
 
-          <Jumbotron id="landingpage" fluid className="text-xs-center">
+          <Jumbotron href="#home" id="landingpage" fluid className="text-xs-center">
             <Container>
             <center>
               <img src={Logo} alt="Logo" />
@@ -34,44 +46,79 @@ class UnauthenticatedApp extends React.Component {
             </Container>
           </Jumbotron>
 
-          <Jumbotron id="mission" fluid className="text-xs-center">
-            <Container>
-            <center>
-              <h1> Our Mission </h1>
-              <p> DevMatch is an app designed to connect current and aspiring software engineers seeking to improve their skills in a certain area with mentors who have real-world experience and are willing to share their knowledge. By allowing users to post their availability and seek out mentors by skill set, DevMatch is aiming to make the way we learn and teach a more open and communal experience. </p>
-              </center>
-            </Container>
-          </Jumbotron>
 
+          <ScrollableAnchor id={'mission'}>
+
+            <Jumbotron id="mission" href="#mission" fluid className="text-xs-center">
+              <Container>
+              <center>
+                <h1> Our Mission </h1>
+                <p> DevMatch is an app designed to connect current and aspiring software engineers seeking to improve their skills in a certain area with mentors who have real-world experience and are willing to share their knowledge. By allowing users to post their availability and seek out mentors by skill set, DevMatch is aiming to make the way we learn and teach a more open and communal experience. </p>
+                </center>
+              </Container>
+            </Jumbotron>
+
+          </ScrollableAnchor>
+
+          <ScrollableAnchor id={'tech'}>
           <Jumbotron id="tech" fluid className="text-xs-center">
-            <Container>
-            <center>
-              <h1> Tech Stack </h1>
-              <p> What we used to build this application</p>
-                <div className="icon">
-                  <i className="devicon-html5-plain-wordmark" ></i>
-                  <i className="devicon-css3-plain-wordmark"></i>
-                  <i className="devicon-javascript-plain"></i>
-                  <i className="devicon-ruby-plain-wordmark"></i>
-                  <i className="devicon-react-original-wordmark"></i>
-                  <i className="devicon-rails-plain-wordmark"></i>
-                  <i className="devicon-postgresql-plain-wordmark"></i>
-                </div>
+              <Container>
+              <center>
+                <h1> Tech Stack </h1>
+                <p> What we used to build this application</p>
+                  <div className="icon">
+                    <i className="devicon-html5-plain-wordmark" ></i>
+                    <i className="devicon-css3-plain-wordmark"></i>
+                    <i className="devicon-javascript-plain"></i>
+                    <i className="devicon-ruby-plain-wordmark"></i>
+                    <i className="devicon-react-original-wordmark"></i>
+                    <i className="devicon-rails-plain-wordmark"></i>
+                    <i className="devicon-postgresql-plain-wordmark"></i>
+                  </div>
+                </center>
+              </Container>
+            </Jumbotron>
+          </ScrollableAnchor>
 
-
-              </center>
-            </Container>
-          </Jumbotron>
-
+          <ScrollableAnchor id={'about'}>
           <Jumbotron id="about" fluid className="text-xs-center">
             <Container>
             <center>
               <h1> About Us</h1>
               <p> DevMatch is an app designed to connect current and aspiring software engineers seeking to improve their skills in a certain area with mentors who have real-world experience and are willing to share their knowledge. By allowing users to post their availability and seek out mentors by skill set, DevMatch is aiming to make the way we learn and teach a more open and communal experience. </p>
               </center>
+              <Container>
+                <Row>
+                  <Col xs={6} md={3}>
+                  <center>
+                    <Image src={Profile} roundedCircle />
 
+                    <h2>Elshaday</h2>
+                    </center>
+                  </Col>
+                  <Col xs={6} md={3}>
+                  <center>
+                    <Image src={Melkam} roundedCircle />
+                    <h2>Melkam</h2>
+                  </center>
+                  </Col>
+                  <Col xs={6} md={3}>
+                  <center>
+                    <Image src={Jerrod} roundedCircle />
+                    <h2>Jerrod</h2>
+                    </center>
+                  </Col>
+                  <Col xs={6} md={3}>
+                  <center>
+                    <Image src={Dante} roundedCircle />
+                    <h2>Dante</h2>
+                    </center>
+                  </Col>
+                </Row>
+              </Container>
             </Container>
           </Jumbotron>
+          </ScrollableAnchor>
 
           <div>
 
